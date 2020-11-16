332f83bdf7d87ebddff8a383d4261d43399ad211
mkoenke <mkoenke@sas.upenn.edu>
2020-11-15 19:50:00 -0500

    completed

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your code here!
+// // Write your code here!
+// let element = document.createElement('div')
+// document.body.appendChild(element)
+// let ul = document.createElement('ul')
+// for (let i = 0; i < 3; i++) {
+//     let li = document.createElement('li')
+//     li.innerHTML = (i + 1).toString()
+//     ul.appendChild(li)
+// }
+// element.appendChild(ul)
+
+// // let element = document.querySelector("p#greeting");
+// // element.innerHTML = "Hello, DOM!" 
+// element.style.backgroundColor = '#27647B';
+// element.style.textAlign = 'center'
+// ul.style.textAlign = 'left'
+// ul.removeChild(ul.querySelector('li:nth-child(2)'))
+// element.remove()
+
+let mainmain = document.querySelector('main#main')
+mainmain.remove()
+
+let newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.innerHTML = "Mary is the champion"
+newHeader.className = "victory"
